04806867831472c7a8088e6c878325702eb6c41c
Rupert McKay <rupert.mckay@dotcontrol.com>
2020-12-03 16:02:53 +0100

    Now with SASS!

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.scss'
 
 const Home = (): JSX.Element => (
   <div className={styles.container}>
