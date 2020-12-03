be7ba3aa82db1884214bc82b89c652cafa7d9cac
Rupert McKay <rupert.mckay@dotcontrol.com>
2020-12-03 14:49:57 +0100

    Now with React linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,19 +6,26 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:jest/recommended',
+    'plugin:react/recommended',
   ],
   root: true,
   parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
   plugins: [
     '@typescript-eslint',
     'jest',
+    'react',
   ],
-  overrides: [
-    {
-      files: ['*.js', '*.jsx'],
-      rules: {
-        '@typescript-eslint/...': 'off',
-      }
-    }
-  ]
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
 }
