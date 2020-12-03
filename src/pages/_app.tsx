bea22ff6e4c94a584b458783cc8c171c9bedfa24
Rupert McKay <rupert.mckay@dotcontrol.com>
2020-12-03 14:26:38 +0100

    Prefer arrow function components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,6 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />
-}
+const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => (<Component {...pageProps} />)
 
 export default MyApp
