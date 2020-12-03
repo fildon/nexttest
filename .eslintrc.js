912d45bf2231fa170b58ee7d7e0c47c7d7984dbc
Rupert McKay <rupert.mckay@dotcontrol.com>
2020-12-03 14:29:36 +0100

    Jest linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,11 +5,13 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:jest/recommended',
   ],
   root: true,
   parser: '@typescript-eslint/parser',
   plugins: [
     '@typescript-eslint',
+    'jest',
   ],
   overrides: [
     {
