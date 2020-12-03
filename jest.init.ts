8413693a5f376356416e326e010d53f85489344a
Rupert McKay <rupert.mckay@dotcontrol.com>
2020-12-03 14:11:24 +0100

    Even more typescript support

--- jest.init.ts
+++ jest.init.ts
@@ -1,6 +1,2 @@
-// Binds React to the global scope for testing convenience
-import React from 'react'
-global.React = React
-
 // Extends Jest expectations with additional dom-themed assertions
 import '@testing-library/jest-dom'
